e627e499ba76eee6ac0ae803e0eae77fb7b01168
ziadadel001 <ziadadel00120@gmail.com>
2026-02-23 05:01:41 +0200

    db

--- database/migrations/2026_02_22_052452_create_customers_table.php
+++ database/migrations/2026_02_22_052452_create_customers_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('mobile')->nullable();
+            $table->string('email')->nullable();
+            $table->text('notes')->nullable();
             $table->timestamps();
         });
     }
